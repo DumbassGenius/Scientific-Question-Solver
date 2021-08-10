a9f449eb93270a94619a09cbb75d8ec938115855
DumbassGenius <sebi.mo@icloud.com>
2021-08-10 00:47:41 -0700

    Inclusion of comments

--- Equation Scripts/Expression class.py
+++ Equation Scripts/Expression class.py
@@ -1,22 +1,24 @@
+
 def root(x,y):
     return x**(1/y)
 
-
+"Equates each operator to a function. // is not floor div but a root, much in the same way ** is power"
 operators = {'+':add,'-':sub,'*':mul,'/':truediv,'**':pow,'//':root}
 
 
 
 class Expression(object):
-    """docstring for Equation."""
+    """This class allows you to represent mathematical expressions as a list of tuples that contain a value and operand."""
 
+    "The init creates the list of operands and the set of variables"
     def __init__(self,term=None):
+        self.eList=[]
+        self.variables = set()
         if term:
-            self.eList=[('+',term)]
-            self.variables = {term}
-        else:
-            self.eList=[]
-            self.variables = set()
+            self + term
 
+    "This is the function to add a new operator. Using the operators merely applies this function with the operator in question as the operator input"
+    "It merely appends a tuple to eList that contains the operator and operand. If the operand is a variable or expression it adds them to the variables"
     def ApplyOperator(self,operator,operand):
         exp = self.eList
         if not (operator in list(operators.keys())):
@@ -24,14 +26,17 @@ class Expression(object):
         if isinstance(operand,str):
             self.variables.add(operand)
         elif isinstance(operand,Expression):
-            self.variables.add(operand.variables)
+            self.variables= self.variables.union(operand.variables)
         else:
             operand = float(operand)
         exp.append((operator,operand))
         return self
 
+    "This goes through each instance in eList, turns variables into the value given by the input, applies itself to expressions within eList,"
+    "then applies the function equavalent to the given operator to the result and the value"
     def Equate(self,**values):
         if not self.variables.issubset(set(values.keys())):
+            print(self.variables,values.keys())
             raise Exception("Not all variables contained in values")
         result = 0
         for op in self.eList:
@@ -39,29 +44,22 @@ class Expression(object):
                 operand = values[op[1]]
             elif isinstance(op[1],Expression):
                 operand = op[1].Equate(**values)
-            elif isinstance(op[1],int):
-                operand = op[1]
+            elif isinstance(op[1],float) or isinstance(op[1],int):
+                operand = float(op[1])
+            result = operators[op[0]](result,operand)
+        return result
 
 
-
+    "Turns operators such as +,-,*,/ into the apply operator function respectively"
     def __add__(self,operand):
         return self.ApplyOperator('+',operand)
     def __sub__(self,operand):
         return self.ApplyOperator('-',operand)
     def __mul__(self,operand):
         return self.ApplyOperator('*',operand)
-    def __div__(self,operand):
+    def __truediv__(self,operand):
         return self.ApplyOperator('/',operand)
     def __pow__(self,operand):
         return self.ApplyOperator('**',operand)
-    def __truediv__(self,operand):
+    def __floordiv__(self,operand):
         return self.ApplyOperator('//',operand)
-
-
-
-# ax^3-bx^2 +cx-6
-
-a = Expression('x')+'y'
-print({'x', 'y'}=={'x', 'y'})
-
-print(a.Equate(x=1,y=2))
